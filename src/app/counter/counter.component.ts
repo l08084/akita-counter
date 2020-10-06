3c108df20dea4fa7da51eb55feb1accb8c1c6843
l08084 <l08084.1989@gmail.com>
2020-10-06 19:36:37 +0900

    update component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { CounterService } from './state/counter.service';
+import { CounterQuery } from './state/counter.query';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent implements OnInit {
+  readonly count$: Observable<number>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private counterService: CounterService,
+    private counterQuery: CounterQuery
+  ) {
+    this.count$ = this.counterQuery.select('count');
   }
 
+  ngOnInit() {}
 }

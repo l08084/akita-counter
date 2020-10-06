c19cdc92fb659d292b618970f4aadf84cb17c88a
l08084 <l08084.1989@gmail.com>
2020-10-06 13:29:32 +0900

    add counter route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'counter', component: CounterComponent },
+  { path: '', redirectTo: '/counter', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

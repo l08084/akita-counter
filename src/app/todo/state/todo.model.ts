8cc378a1fe047a2d090b39b45da12f5cfbdc9a77
l08084 <l08084.1989@gmail.com>
2020-10-08 18:26:59 +0900

    add query, model, store

---
+++ src/app/todo/state/todo.model.ts
@@ -0,0 +1,7 @@
+export interface Todo {
+  title: string;
+}
+
+export function createTodo(params: Partial<Todo>) {
+  return {} as Todo;
+}
---
+++ src/app/todo/state/todo.store.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
+import { Todo } from './todo.model';
+
+export interface TodoState extends EntityState<Todo, number> {}
+
+@Injectable({ providedIn: 'root' })
+@StoreConfig({ name: 'todo' })
+export class TodoStore extends EntityStore<TodoState> {
+  constructor() {
+    super();
+  }
+}
